6edd753309966b37160e8478328e124bc6cd02f4
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-05-27 14:59:45 +0200

    Working on backend

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ const io = require('socket.io')(server, {
         methods: ['GET', 'POST']
     }
 })
+
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    res.send('Server is running!')
+})
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id)
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callended')
+    })
+
+    socket.io('calluser', ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit('calluser')
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
