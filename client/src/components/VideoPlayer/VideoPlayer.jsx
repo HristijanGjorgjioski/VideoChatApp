19eb08aa63bee22d388f2c8a02237dfb1cccd5c4
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-05-30 18:14:27 +0200

    Working on VideoPlayer functinality

--- client/src/components/VideoPlayer/VideoPlayer.jsx
+++ client/src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,17 +1,25 @@
 import { Grid, Paper, Typography } from '@material-ui/core'
-import React from 'react'
+import React, { useContext } from 'react'
+import { SocketContext } from '../../contexts/SocketContext'
 
 import useStyles from './styles'
 
 const VideoPlayer = () => {
-    const {  } = {}
+    const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext)
     const classes = useStyles()
 
     return (
         <Grid container className={classes.gridContainer}>
             <Paper className={classes.paper}>
                 <Grid item xs={12} md={6}>
+                    <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
+                    <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
+                </Grid>
+            </Paper>
+            <Paper className={classes.paper}>
+                <Grid item xs={12} md={6}>
                     <Typography variant="h5" gutterBottom>Name</Typography>
+                    <video playsInline ref={null} autoPlay className={classes.video} />
                 </Grid>
             </Paper>
         </Grid>
