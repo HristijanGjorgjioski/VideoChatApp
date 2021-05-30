c375433af494a6eff7c63a97e2d1d838e9820471
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-05-30 18:34:41 +0200

    Options functinality is almost done

--- client/src/components/Options/Options.jsx
+++ client/src/components/Options/Options.jsx
@@ -2,9 +2,9 @@ import React, { useContext, useState } from 'react'
 
 import useStyles from './styles'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
-import { Assigment, Phone, PhoneDisabled } from '@material-ui/icons'
+import { Assignment, Phone, PhoneDisabled } from '@material-ui/icons'
 import { SocketContext } from '../../contexts/SocketContext'
-import { Container, Paper } from '@material-ui/core'
+import { Button, Container, Grid, Paper, TextField, Typography } from '@material-ui/core'
 
 const Options = ({ children }) => {
     const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } = useContext(SocketContext)
@@ -13,8 +13,20 @@ const Options = ({ children }) => {
 
     return (
         <Container className={classes.container}>
-            <Paper>
-                
+            <Paper elevation={10} className={classes.paper}>
+                <form className={classes.root} noValidate autoComplete="off">
+                    <Grid container className={classes.gridContainer}>
+                        <Grid item xs={12} md={6} className={classes.padding}>
+                            <Typography gutterBottom variant="h6">Account Info</Typography>
+                            <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
+                            <CopyToClipboard text={me} className={classes.margin}>
+                                <Button variant="contained" color="primary" fullWidth startIcon={<Assignment fontSize="large" />}>
+                                    
+                                </Button>
+                            </CopyToClipboard>
+                        </Grid>
+                    </Grid>
+                </form>
             </Paper>
         </Container>
     )
