e5a5e1c679a28937f7ddb8a960bbc5e0c7a421c2
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-05-30 18:26:56 +0200

    Working on Options functinality and styles

--- client/src/components/Options/Options.jsx
+++ client/src/components/Options/Options.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+
+import useStyles from './styles'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { Assigment, Phone, PhoneDisabled } from '@material-ui/icons'
+import { SocketContext } from '../../contexts/SocketContext'
+import { Container, Paper } from '@material-ui/core'
 
 const Options = ({ children }) => {
+    const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } = useContext(SocketContext)
+    const [idToCall, setIdToCall] = useState('')
+    const classes = useStyles()
+
     return (
-        <div>
-            {children}
-        </div>
+        <Container className={classes.container}>
+            <Paper>
+                
+            </Paper>
+        </Container>
     )
 }
 
