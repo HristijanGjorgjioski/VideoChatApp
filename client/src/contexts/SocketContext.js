3144fe40d14bd1ddded55b530bd0afcdb829f0e7
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-06-01 12:57:42 +0200

    Chaning server URL

--- client/src/contexts/SocketContext.js
+++ client/src/contexts/SocketContext.js
@@ -5,7 +5,8 @@ import Peer from 'simple-peer'
 
 const SocketContext = createContext()
 
-const socket = io('http://localhost:5000')
+// const socket = io('http://localhost:5000')
+const socket = io('https://webrtc-server-gjorgjioski.herokuapp.com/')
 
 const ContextProvider = ({ children }) => {
     const [stream, setStream] = useState(null)
